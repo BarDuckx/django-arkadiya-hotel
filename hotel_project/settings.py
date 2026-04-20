70d603e8164a1814d5619a46401e15b67d6db8d4
Borisov Danil <danilborisov039@gmail.com>
2026-04-20 18:39:44 +0900

    Fix CSRF for Railway deployment

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-wte51_)%+mp=v0!+lq&hnfaad!%&0mvzk%2w)8z4alkuoju%&l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['.railway.app', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app', 'http://*.railway.app']
 
 
 # Application definition
